be999cedf4c9cd277f73e8f9e9c2118b4fe6959b
Niv Sardi <xaiki@evilgiggle.com>
2018-04-14 22:37:50 -0300

    quick wireframe
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import style from './styl/style.styl';
+import {Navbar, Dropdowns, Buttons} from 'butter-base-components';
 import ActionBar from './components/action-bar';
+
+let {Dropdown} = Dropdowns
+let {Button} = Buttons
 
 let CloseButton = (props) => (props)
 
-export default class MovieDetails extends Component {
-    render() {
-        let {state, props} = this;
-        return (
-            <div className={style.layout}>
-                    <ActionBar />
+let LegendedButton = ({children, title}) => (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+        {children}
+        {title}
+    </div>
+)
+
+let ToolBar = ({}) => (
+    <div>
+        <Button title="Add to Bookmarks" icon="favorite"/>
+        <Button title="Seen" icon="visibility"/>
+    </div>
+)
+
+let PlayButtons = ({type, torrents, subtitles, ...props}) => (
+    <div style={{display: 'flex'}}>
+        <LegendedButton title={`Play ${type}`}><Button title="Watch Now" /></LegendedButton>
+        <LegendedButton title="Subtitles"><Dropdown options={Object.keys(subtitles)}/></LegendedButton>
+        <LegendedButton title="Quality"><Dropdown options={Object.keys(torrents)}/></LegendedButton>
+
+        <LegendedButton title="Streamer"><Dropdown options={['butter', 'vlc', 'mpv']}/> </LegendedButton>
+    </div>
+)
+
+let Info = ({year, runtime, genres, rating, ...props}) => (
+    <div>
+        {year} · {runtime} mins · {genres[0]} · {rating}
+    </div>
+)
+
+let MovieDetails = ({title, synopsis, cover, backdrop, ...props}) => (
+    <div className={style.layout}>
+        <Navbar goBack="/" title="Go Back" toolbar={<ToolBar />} />
+
+        <div style={{display: 'flex', backgroundImg: `url(${backdrop})`}}>
+            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                <h1>{title}</h1>
+                <Info {...props}/>
+                <p style={{flexGrow: 2}}>
+                    {synopsis}
+                </p>
+                <PlayButtons {...props}/>
             </div>
-        );
-    }
+            <img src={cover} style={{padding: '4vh', borderRadius: '5px'}}/>
+        </div>
+    </div>
+)
+export {
+    MovieDetails as default
 }
--- test/data.js
+++ test/data.js
@@ -1,1 +1,53 @@
-export default {}
+export default {
+    "imdb_id": "tt1839578",
+    "title": "Person of Interest",
+    "year": "2010",
+    "genres": [
+        "thriller", "conspiracy", "indie"
+    ],
+    "rating": "8.5",
+    "runtime": 108,
+    "image": "http://vodo.net/media/posters/poster_personofinterest.jpg",
+    "cover": "http://vodo.net/media/posters/poster_personofinterest.jpg",
+    "backdrop": "http://vodo.net/media/posters/poster_personofinterest.jpg",
+    "subtitles": {
+        "english": {
+            url: "http://blah.srt/"
+        },
+        "spanish": {
+            url: "http://blah.srt/"
+        },
+        "french": {
+            url: "http://blah.srt/"
+        },
+        "german": {
+            url: "http://blah.srt/"
+        }
+    },
+    "torrents": {
+        "720p": {
+            "url": "http://vodo.net/media/torrents/h/Person.of.Interest.2010.720p.x264-VODO.mp4.torrent",
+            "size": 147,
+            "filesize": "0.0M",
+            "seed": 0,
+            "peer": 0,
+            "health": "bad"
+        },
+        "1080p": {
+            "url": "http://vodo.net/media/torrents/h/Person.of.Interest.2010.720p.x264-VODO.mp4.torrent",
+            "size": 147,
+            "filesize": "0.0M",
+            "seed": 0,
+            "peer": 0,
+            "health": "bad"
+        }
+    },
+    "trailer": false,
+    "synopsis": "A veteran of the war in Iraq, and self-proclaimed American Patriot, becomes convinced that the federal government is setting him up as the fall guy in a soon-to-be terrorist attack on American soil.",
+    "type": "movie",
+    "_id": "8O3XXwsfu47svXaS",
+    "provider": "vodo",
+    "watched": false,
+    "bookmarked": false,
+    "health": false
+}
